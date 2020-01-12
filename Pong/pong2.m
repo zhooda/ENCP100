5f4c607ce7012dcfced35133f778c75fd3c82a77
Zeeshan Hooda <zeeshanhooda@icloud.com>
2020-01-12 01:44:22 -0700

    Started proper 2 paddle pong game.

---
+++ Pong/pong2.m
@@ -0,0 +1,128 @@
+clear; clc; close all;
+
+% ----------------------------- Setup ------------------------------- %
+
+% --- Window Setup --- %
+window = figure;
+window.Name = 'Pong';
+window.NumberTitle = 'off';
+window.ToolBar = 'none';
+window.MenuBar = 'none';
+window.Color = [.6 .6 .8];
+window.KeyPressFcn = @keyboardFunction;
+
+% --- Axis Setup --- %
+
+axis = axes;
+axis.Color = 'black';
+axis.XLim = [0 100];
+axis.YLim = [-5 100];
+axis.XTick = [];
+axis.YTick = [];
+axis.Position = [.1 .1 .8 .8];
+
+% --- Score Setup --- %
+
+leftScore = 0;
+rightScore = 0;
+
+leftScoreLabel = text(104, 99, '0', 'color', 'white', 'fontsize', 18);
+leftScoreLabel.String = num2str(leftScore);
+
+rightScoreLabel = text(-6.5, 99, '0', 'color', 'white', 'fontsize', 18);
+rightScoreLabel.String = num2str(rightScore);
+
+leftScoreTitle = text;
+leftScoreTitle.String = 'Score';
+leftScoreTitle.Position = [-9 105];
+leftScoreTitle.Color = 'white';
+leftScoreTitle.FontSize = 14;
+
+rightScoreTitle = text;
+rightScoreTitle.String = 'Score';
+rightScoreTitle.Position = [101 105];
+rightScoreTitle.Color = 'white';
+rightScoreTitle.FontSize = 14;
+
+% --- Ball Setup --- %
+
+ballPos = [50, 45];
+ballVel = [1, 1];
+
+ball = line;
+ball.LineWidth = 20;
+ball.Marker = '.';
+ball.XData = ballPos(1);
+ball.YData = ballPos(2);
+ball.MarkerSize = 20;
+ball.Color = 'white';
+
+% --- Paddle Setup --- %
+
+global leftPaddleCenter;
+global rightPaddleCenter;
+leftPaddleCenter = 45;
+rightPaddleCenter = 45;
+
+leftPaddle = line;
+leftPaddle.XData = [0 0];
+leftPaddle.YData = [leftPaddleCenter - 8, leftPaddleCenter + 8];
+leftPaddle.LineWidth = 6;
+leftPaddle.Color = 'white';
+
+rightPaddle = line;
+rightPaddle.XData = [100 100];
+rightPaddle.YData = [rightPaddleCenter - 8, rightPaddleCenter + 8];
+rightPaddle.LineWidth = 6;
+rightPaddle.Color = 'white';
+
+% ------------------------------ Loop ------------------------------- %
+
+while 1
+   
+   % start timer
+   tic;
+   
+   % top and bottom collision
+   if ballPos(2) <= -5 || ballPos(2) >= 100
+      ballVel(2) = -ballVel(2); 
+   end
+   if ballPos(1) <= 0
+       ballPos = [50 50];
+       ballVel = [1, 1];
+       leftScore = leftScore + 1;
+   elseif ballPos(1) >= 100
+       ballPos = [50, 50];
+       ballVel = [-1, 1];
+       rightScore = rightScore + 1;
+   end
+   
+   ballPos = ballPos + ballVel;
+   
+   ball.XData = ballPos(1);
+   ball.YData = ballPos(2);
+   
+   leftPaddle.YData = [ballPos(2) - 8, ballPos(2) + 8];
+   rightPaddle.YData = [rightPaddleCenter - 8, rightPaddleCenter + 8];
+   
+   leftScoreLabel.String = num2str(leftScore);
+   rightScoreLabel.String = num2str(rightScore);
+   
+   % 60 fps
+   pause(1/60 - toc);
+end
+
+% ---------------------------- Functions ---------------------------- %
+
+function keyboardFunction(figure, event)
+%     global leftPaddleCenter;
+    global rightPaddleCenter;
+    
+%     if strcmp(event.Key, 'w'), leftPaddleCenter = leftPaddleCenter + 5; end
+%     if strcmp(event.Key, 's'), leftPaddleCenter = leftPaddleCenter - 5; end
+    if strcmp(event.Key, 'uparrow'), rightPaddleCenter = rightPaddleCenter + 5; end
+    if strcmp(event.Key, 'downarrow'), rightPaddleCenter = rightPaddleCenter - 5; end
+    
+    if strcmp(event.Key, 'escape'), close all; return; end
+    
+end
